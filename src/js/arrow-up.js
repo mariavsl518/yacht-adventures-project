aa7d7a9eda8dece6ea5c9e558a42710a7e9566b4
Veronika Lykhovoz <veronikamaltseva99@gmail.com>
2023-07-30 17:17:16 +0300

    UPD btn
    Зробила кнопку з джава

--- src/js/arrow-up.js
+++ src/js/arrow-up.js
@@ -13,3 +13,18 @@ function toggleTopButton() {
     document.getElementById('back-to-up').classList.add('d-none');
   }
 }
+
+// Get the "Back to Top" button element
+const backToTopButton = document.getElementById('backToTopBtn');
+
+// Function to show or hide the button based on the user's scroll position
+function toggleBackToTopButton() {
+    if (window.scrollY > 0) {
+        backToTopButton.style.display = 'block'; // Show the button when scrolling down
+    } else {
+        backToTopButton.style.display = 'none'; // Hide the button when at the top of the page
+    }
+}
+
+// Event listener to trigger the function when the user scrolls
+window.addEventListener('scroll', toggleBackToTopButton);
